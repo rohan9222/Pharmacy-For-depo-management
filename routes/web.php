8ccfaedb101719a7c5dcd6231bc468effe0a5152
rohan9222 <rohan9222@gmail.com>
2025-01-15 16:33:07 +0600

    2nd

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,8 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+// use App\Http\Controllers\{UserProfileController};
+use App\Http\Controllers\Admin\{RoleController,UserController};
 Route::get('/', function () {
     return view('welcome');
 });
@@ -14,4 +15,14 @@ Route::middleware([
     Route::get('/dashboard', function () {
         return view('dashboard');
     })->name('dashboard');
+// profile
+    // Route::get('/user/profile', [UserProfileController::class, 'index'])->name('user.profile');
+    // Route::post('/user/profile/upload', [UserProfileController::class, 'uploadFile'])->name('user.profile.upload');
+    // Route::get('/user/profile/update', [UserProfileController::class, 'update'])->name('user.profile.update');
+    // Route::get('/user/password/update', [UserProfileController::class, 'updatePassword'])->name('user.password.update');
+// user and role management
+    Route::resources([
+        'roles' => RoleController::class,
+        'users' => UserController::class,
+    ]);
 });
